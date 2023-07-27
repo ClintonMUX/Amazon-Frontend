ed0b5bd4747ec6744cb257094cf949445016ee45
unknown <clintonmburli@gmail.com>
2023-07-27 20:36:47 +0100

    Added Products

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,45 +1,4 @@
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87,
 
-  },
-  priceCents: 1090,
-},
-{
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4.0,
-    count: 127,
-
-  },
-  priceCents: 2095,
-},
-{
-image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56,
-
-  },
-  priceCents: 799,
-},
-{
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-      stars: 4.5,
-      count: 56,
-  
-    },
-    priceCents: 799,
-  }
-
-];
 
 let productsHTML = '';
 
@@ -83,12 +42,12 @@ products.forEach( (product) =>{
 
         <div class="product-spacer"></div>
 
-        <div class="added-to-cart">
+        <div class="added-to-cart"  >
           <img src="images/icons/checkmark.png" />
           Added
   </div>
 
-  <button class="add-to-cart-button button-primary">Add to Cart</button>
+  <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name = "${product.name}">Add to Cart</button>
 </div>`;
 
 
@@ -98,3 +57,24 @@ products.forEach( (product) =>{
 console.log(productsHTML);
 
 document.querySelector('.js-product-grid').innerHTML = productsHTML; 
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) =>{
+
+  button.addEventListener('click', () =>{
+   const productName =  button.dataset.productName;
+   
+   cart.forEach((item) =>{
+   
+    if(productName === item.productName){
+      
+    }
+
+
+   });
+   cart.push({
+    productName: productName,
+    quantity: 1
+   })
+console.log(cart);
+  });
+})
